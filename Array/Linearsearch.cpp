c8d698e229547aa571cabc3ca8147d5035e428f4
gagan8055r <123857923+gagan8055r@users.noreply.github.com>
2023-05-16 14:57:27 +0530

    Create Linearsearch.cpp

---
+++ Array/Linearsearch.cpp
@@ -0,0 +1,17 @@
+int Linear(Array &arr,int key)
+{
+    int i;
+    for(i=0;i<arr.length;i++)
+    
+        {
+            
+    if(key==arr.A[i])
+    {
+    return i;
+    }
+        }
+        
+            
+   return -1;
+        
+}
