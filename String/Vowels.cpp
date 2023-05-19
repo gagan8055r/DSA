dfd19b060ea3b37bb0fc8488e858990e11f572e2
gagan8055r <123857923+gagan8055r@users.noreply.github.com>
2023-05-19 23:53:30 +0530

    Create Vowels.cpp

---
+++ String/Vowels.cpp
@@ -0,0 +1,12 @@
+  char name[]="gagan";
+  int i,count;
+
+for(i=0;name[i]!='\0';i++)
+{
+    if(name[i]='a' || name[i]='e' || name[i]='i' || name[i]='o' || name[i]='u' ) ////vowels in the words
+    {
+        count++;
+        
+    }
+}
+cout<<count<<endl;
