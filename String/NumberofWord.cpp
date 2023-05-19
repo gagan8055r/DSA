da0f3b977d29a955cc49b3dc157020a804733c95
gagan8055r <123857923+gagan8055r@users.noreply.github.com>
2023-05-19 23:54:04 +0530

    Create NumberofWord.cpp

---
+++ String/NumberofWord.cpp
@@ -0,0 +1,9 @@
+ char name[]="How are you in the sentence of the work in";
+    int i,count;
+     for(i=0;name[i]!='\0';i++)
+     {   
+         if(name[i]==' ')           ///number of word
+         {
+             count++;
+         }
+     }
