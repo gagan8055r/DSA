6b10c2df68d72afef61a22ba7a520fcbdf0dc099
gagan8055r <123857923+gagan8055r@users.noreply.github.com>
2023-05-23 21:58:54 +0530

    Create RemoveDupSort.cpp

---
+++ GFG/RemoveDupSort.cpp
@@ -0,0 +1,17 @@
+int main()
+
+{
+    int A[5]={2,2,2,2,5};
+    int B[5];
+    int i,j;
+    int n=5;
+    for(i=0;i<n;i++)
+    {
+        if(A[i]!=A[i+1])
+        {
+            B[i]=A[i];
+         for(j=i;j<=i;j++)
+        cout<<B[j]<<endl;
+       }
+    }
+}
