c27691cb58ff5e5af1c847bdc64315cf4ac6858f
gagan8055r <123857923+gagan8055r@users.noreply.github.com>
2023-07-05 17:55:59 +0530

    Create Keypair.js

---
+++ GFG/Keypair.js
@@ -0,0 +1,19 @@
+const arr=[1,4,45,6,10,8]
+ const num=16
+
+
+
+    for( let i=0;i<arr.length;i++)
+    {
+        for(j=i+1;j<arr.length;j++)
+        {
+            if(arr[i]+arr[j]==num)
+            {
+                console.log('yes');
+          
+            }
+            
+            
+        }
+        
+    }
