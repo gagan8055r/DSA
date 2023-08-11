e8165d39eaeaa39c90d527abc4be9c76d756be54
gagan8055r <123857923+gagan8055r@users.noreply.github.com>
2023-08-12 01:01:41 +0530

    Update stl.cpp

--- STL/stl.cpp
+++ STL/stl.cpp
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <iostream>
 #include<queue> 
 #include<set>
@@ -49,4 +41,74 @@ int main()
     //     cout<<i<<endl;
     // }
     
-   
+    // cout<<"this is the value"<<" "<<s.count(12)<<endl;     ////it will give binary if true 1 if not 0
+    
+    
+    
+    
+    // priority_queue<int,vector<int>,greater<int>> mini;
+    
+    
+    
+    
+    
+    // max.push(1);
+    // max.push(70);
+    // max.push(4);
+    // max.push(2);
+    // int n=max.size();
+    // for(int i=0;i<n;i++)
+    // {
+    //     cout<<max.top()<<endl;
+    //     max.pop();
+    // }
+    
+    
+    // mini.push(22);
+    //  mini.push(12);
+    //   mini.push(8);
+    //   mini.push(1);
+       
+       
+    //   int m=mini.size();
+    //   for(int j=0;j<m;j++)
+    //   {
+    //       cout<<mini.top()<<endl;
+    //       mini.pop();
+    //   }
+    
+    
+    
+    
+    // map<string,int> m;
+    // map<int ,string> m;
+    
+    
+    // m[2]="gagan";
+    // m[34]="qwerty";
+    // m[3]="king";
+    
+    
+    
+    
+    
+    // cout<<m.count(-34)<<endl;
+    
+    
+    // m.erase(34);
+    //  for(auto i :m)               ///for int in belongs to m  this is read as;;;
+    
+// //   v.push_back(13);
+//      v.push_back(5);
+//      v.push_back(70);
+//   v.push_back(2);
+//      v.push_back(12);
+    
+//     for(auto i:v)
+//     {
+//         cout<<i<<endl;
+//     }
+    
+//     cout<<binary_search(v.begin(),v.end(),70)<<endl;
+
+
