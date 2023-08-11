3b4ac1cbdc0cc0b57b259b1824a9d4112b91a659
gagan8055r <123857923+gagan8055r@users.noreply.github.com>
2023-08-12 01:00:42 +0530

    Create stl.cpp

---
+++ STL/stl.cpp
@@ -0,0 +1,52 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+#include<queue> 
+#include<set>
+#include<map>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+    // priority_queue<int> max;
+   
+                       //// no modification in set and no duplicate and always store the value in ascending order
+    
+    //  set<int> s;  
+    
+    // s.insert(12);
+    // s.insert(9);
+    // s.insert(4);
+    // s.insert(5);
+    // // s.insert(5);
+    // // s.insert(4);
+    
+    // for(int i :s)
+    // {
+    //     cout<<i<<endl;
+    // }
+    
+    // set<int>::iterator itr=s.find(12);
+    
+    // cout<<*itr<<endl;
+    
+    
+    // set<int>::iterator it=s.begin();        //use itertor for this
+    // it++;
+    // s.erase(it);
+    
+    
+    // for(int i :s)
+    // {
+    //     cout<<i<<endl;
+    // }
+    
+   
